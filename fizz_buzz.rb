b9ccf74daa517ad0be2f57b1203dc8c1a2828fab
佐久間　祥 <sakumasho@sakumasachinoMacBook-Pro.local>
2020-03-08 01:16:57 +0900

    [Finish] 2-9

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,1 +1,22 @@
-def
+def fizz_buzz(number)
+	if number % 15 == 0
+		puts "Fizz_Buzz"
+
+	elsif number % 3 == 0
+		puts "Fizz"
+
+	elsif number  % 5 == 0
+		puts "Buzz"
+
+	else
+		number.to_i
+	end
+end
+ 
+puts "1 以上の数字を入力してください"
+ 
+input = gets.to_i
+ 
+puts '結果は、、、'
+ 
+puts fizz_buzz(input)
